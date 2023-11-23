5f2660e7c7247f4ed5f3261d2e68916d3092bf73
maxafire <maxafire9@gmail.com>
2023-11-23 20:34:18 +0100

    Mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+// Set current year
 const yearEl = document.querySelector(".year");
 if (yearEl !== null) {
   yearEl.textContent = new Date().getFullYear();
 }
+
+// Mobile navigation
+const btnEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+btnEl.addEventListener("click", (e) => {
+  headerEl.classList.toggle("nav-open");
+});
