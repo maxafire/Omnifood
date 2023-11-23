9ed0cf3ed2248e2bb1fa9f31947b2d5c16d0b177
maxafire <maxafire9@gmail.com>
2023-11-23 20:59:26 +0100

    Smooth scrolling

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ const headerEl = document.querySelector("header");
 btnEl.addEventListener("click", (e) => {
   headerEl.classList.toggle("nav-open");
 });
+
+// Smooth scroll animiation (no longer necessary, because scroll-behavior: smooth now works in every browser!)
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href.startsWith("#") && href !== "#") {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
